a26f8dd46d2f49d5839e9046da38f557d6e1ea1a
knoxie-s <darkblue7x@gmail.com>
2024-12-18 00:14:50 +0500

    database connection, migration

---
+++ db/db.go
@@ -0,0 +1,59 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"shobak/models"
+	"shobak/pkg/setting"
+
+	"github.com/kr/pretty"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var db *gorm.DB
+
+// Setup initializes the database instance
+func Setup() {
+	var err error
+
+	dbConfig := setting.Config.DB
+
+	dsn := fmt.Sprintf("host=%s port=%d user=%s "+
+		"dbname=%s sslmode=disable",
+		dbConfig.Host, dbConfig.Port, dbConfig.User, dbConfig.DBName)
+
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
+
+	if err != nil {
+		log.Fatalf("db.Setup err:%w", err)
+	}
+	sqlDB, _ := db.DB()
+	sqlDB.SetMaxOpenConns(100)
+
+	//AutoMigrate
+	autoMigrate()
+	log.Println("DB successfully connected! ")
+}
+
+// CloseDB closes database connection (unnecessary)
+func CloseDB() {
+	sqlDB, err := db.DB()
+	sqlDB.Close()
+	if err != nil {
+		pretty.Logln("Error on closing the DB: ", err)
+	}
+}
+
+func autoMigrate() {
+	for _, model := range []interface{}{
+		(*models.User)(nil),
+	} {
+		dbSilent := db.Session(&gorm.Session{Logger: logger.Default.LogMode(logger.Silent)})
+		err := dbSilent.AutoMigrate(model)
+		if err != nil {
+			log.Fatalf("create model %s : %s", model, err)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -4,15 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"shobak/db"
 	"shobak/pkg/setting"
 	"shobak/routes"
+
+	"github.com/kr/pretty"
 )
 
 func init() {
 	setting.Setup("./config/config.json")
+	db.Setup()
 }
 
 func main() {
+	defer deferFunc()
+
 	endPoint := fmt.Sprintf(":%d", setting.Config.App.Port)
 
 	server := &http.Server{
@@ -24,5 +30,9 @@ func main() {
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		log.Fatal("listen:", err)
 	}
+}
 
+func deferFunc() {
+	pretty.Logln("[MAIN] Work has stopped!")
+	db.CloseDB()
 }
